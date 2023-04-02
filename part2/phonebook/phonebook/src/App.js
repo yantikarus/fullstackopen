a5482726b3337b5035b0c41c9b795c31567b156c
Yanti <wsutr@hotmail.com>
2023-04-02 17:26:41 -0600

    working on showing filtered list

--- part2/phonebook/phonebook/src/App.js
+++ part2/phonebook/phonebook/src/App.js
@@ -3,11 +3,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name:'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   //Handle form input element
   const [newName, setNewName] =useState('')
   const [newNumbers, setNewNumbers] =useState(0)
+
+  let displayNumber = persons.map(x=> <li key={x.name}>{x.name} {x.number}</li>)
+
 
   const addPerson =(event)=> {
     event.preventDefault()
@@ -36,9 +42,24 @@ const App = () => {
   const handleNewNumber= (event)=>{
     setNewNumbers(event.target.value)
   }
+  const handleFilterInput = (event) =>{
+    const search = event.target.value
+    // const filtered = persons.filter(x => x.name.includes(search))
+    // console.log("i am searching for", search)
+    // console.log("result", filtered)
+    displayNumber = persons.filter(x => x.name.includes(search))
+    console.log(displayNumber)
+
+  }
+
+
   return (
      <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input onChange={handleFilterInput}/>
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleInputChange}/>
@@ -53,7 +74,7 @@ const App = () => {
     
       <h2>Numbers</h2>
       <ul>
-      {persons.map(x=> <li key={x.name}>{x.name} {x.number}</li>)}
+      {displayNumber}
       </ul>
     </div>
   )
